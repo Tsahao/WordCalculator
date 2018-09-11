60c4c0305d4f9659e3a4558f79aa881cf484a18f
于泽浩 <tsahao@qq.com>
2018-09-12 00:17:16 +0800

    Initial Commit

---
+++ WordCalculator/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  WordCalculator
+//
+//  Created by Tsahao Yu on 9/12/18.
+//  Copyright © 2018 Tsahao Yu. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}

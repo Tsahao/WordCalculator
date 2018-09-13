3474b7540d0411ecd5bba83cd3f6a814453a018b
于泽浩 <tsahao@qq.com>
2018-09-13 22:02:12 +0800

    Add main, finish countBasic

--- WordCalculator/main.cpp
+++ WordCalculator/main.cpp
@@ -10,38 +10,58 @@
 #include<string>
 using namespace std;
 
+#define SIZE 256
+
+
+
+
 bool isCharacter(char str)
 {
-    if((str >= 'A' && str <= 'z') || (str >='0' && str <= '9'))
-        return true;
-    else
-        return false;
+    return ((str >= 'A' && str <= 'z') || (str >='0' && str <= '9'));
 }
 
-void countBasic(fstream &outfile, int *cnt)
+void countBasic(fstream &outfile, int *cnt)//Basic count mathod
 {
-    char str[256];
-    while(outfile.getline(str,256))
+    char str[SIZE];
+    while(outfile.getline(str,SIZE))
     {
         int tmp = 0;
         
         for(int i=0; i < strlen(str);i++)
         {
             if(str[i] == ' ' || str[i] == '.' || str[i] == ',' || str[i] == '?' || str[i] == '!')
-                cnt[1]++;
+                cnt[1]++;//count the number of words
             
             if(isCharacter(str[i]))
             {
                 cnt[0]++;
                 tmp++;
-            }
+            }//count the number of characters
 
         }
         
-    if(tmp != 0)
-        cnt[2]++;
+    if(tmp != 0) cnt[2]++;//count the numbers of lines
         tmp=0;
     }
     return;
 }
 
+int main()
+{
+    char filename[SIZE];
+    int cnt[3]={0};
+    
+    cout<<"Please input your file path:"<<endl;
+    cin.getline(filename,SIZE);
+    
+    fstream outfile(filename,ios::in);
+    countBasic(outfile, cnt);
+    
+    cout<<"Total number of characters is: "<<cnt[0]<<endl;
+    cout<<"Total number of words is: "<<cnt[1]<<endl;
+    cout<<"Total number of lines is: "<<cnt[2]<<endl;
+    
+    outfile.close();
+    return 0;
+    
+}
